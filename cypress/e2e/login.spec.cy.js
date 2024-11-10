7b600f816def9f91654778c78d3ffb66b243cd86
Nayra Braga Barbosa <nayrabra@gmail.com>
2024-11-09 21:32:06 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,4 @@
-import userData from "../fixtures/userData.json"
+import userData from "../fixtures/userData.json";
 
 describe("Orange HRM Tests", () => {
   const selectorsList = {
@@ -11,9 +11,8 @@ describe("Orange HRM Tests", () => {
   };
 
   it("Login - Sucess", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userSucess.username);
     cy.get(selectorsList.passwordField).type(userData.userSucess.password);
     cy.get(selectorsList.loginButton).click();
@@ -21,9 +20,8 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.dashboardGrid);
   });
   it("Login - Fail", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userFail.username);
     cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
